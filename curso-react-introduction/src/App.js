7f66b5f7caf761e07b62e513eaac6341763d80df
misterdan100 <danielmca60@gmail.com>
2023-10-12 21:45:46 -0500

    Adding search todos feature

--- curso-react-introduction/src/App.js
+++ curso-react-introduction/src/App.js
@@ -19,6 +19,13 @@ function App() {
 
   const completedTodos = todos.filter( todo => !!todo.completed).length;
   const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(( todo ) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    } 
+  );
   
   console.log(searchValue);
 
@@ -32,7 +39,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
