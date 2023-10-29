fffd3c1efd3e274a418bcb855868f4d67ff27644
misterdan100 <danielmca60@gmail.com>
2023-10-28 20:37:24 -0500

    Preparing file to Loading Skeletons Chanllenge

---
+++ curso-react-introduction/src/EmptyTodos/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './EmptyTodos.css';
+
+function EmptyTodos() {
+
+
+  return (
+    <p>Crea tu primer Todo!</p>
+  );
+}
+
+export { EmptyTodos };
---
+++ curso-react-introduction/src/TodosLoading/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TodosLoading.css';
+
+function TodosLoading() {
+
+
+  return (
+    <p>Cargando...</p>
+  );
+}
+
+export { TodosLoading };
