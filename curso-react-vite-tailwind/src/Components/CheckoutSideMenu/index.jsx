964eede279359af1dc908ebda64c38128f18d37f
misterdan100 <danielmca60@gmail.com>
2023-11-12 05:55:47 -0500

    19 adding delete button funtionality from order cart

--- curso-react-vite-tailwind/src/Components/CheckoutSideMenu/index.jsx
+++ curso-react-vite-tailwind/src/Components/CheckoutSideMenu/index.jsx
@@ -1,45 +1,58 @@
-import { XCircleIcon } from '@heroicons/react/24/outline'
-import { useContext, useEffect, useState } from 'react';
-import { ShoppingCartContext } from '../../Context';
-import { OrderCard } from '../OrderCard';
+import { XCircleIcon } from "@heroicons/react/24/outline";
+import { useContext, useEffect, useState } from "react";
+import { ShoppingCartContext } from "../../Context";
+import { OrderCard } from "../OrderCard";
 
 const CheckoutSideMenu = () => {
-    const { isCheckoutSideMenuOpen, closeCheckoutSideMenu, productToShow, cartProducts } = useContext(ShoppingCartContext)
+  const {
+    isCheckoutSideMenuOpen,
+    closeCheckoutSideMenu,
+    productToShow,
+    cartProducts,
+    setCartProducts,
+  } = useContext(ShoppingCartContext);
 
-    return (
-        <aside
-            className={`${isCheckoutSideMenuOpen ? 'opacity-100 right-2' : 'opacity-0 right-[-380px]'} flex flex-col fixed  top-[80px] p-4 border bg-white/60 backdrop-blur-3xl border-gray-400 rounded-lg w-[360px] h-[calc(100vh-90px)] transition-all duration-[3000] overflow-y-auto `}
-        >
-            <div className="flex justify-between items-center pb-6 px-2">
-                <h2 className="font-medium text-xl">My Order</h2>
-                <XCircleIcon
-                    className="h-6 w-6 text-gray-500 cursor-pointer hover:text-red-600 transition-all"
-                    onClick={() => closeCheckoutSideMenu()}
-                />
-            </div>
+  const handleDelete = (id) => {
+    const filteredProducts = cartProducts.filter( product => product.id != id)
+    setCartProducts(filteredProducts);
+  }
 
-            <div className='overflow-y-auto scroll-smooth  [&::-webkit-scrollbar]:w-2
-  [&::-webkit-scrollbar-track]:rounded-full
-  [&::-webkit-scrollbar-track]:bg-gray-100
-  [&::-webkit-scrollbar-thumb]:rounded-full
-  [&::-webkit-scrollbar-thumb]:bg-gray-300'>
-            {
-                cartProducts.map( product => (
-                    <OrderCard
-                        key={product.id}
-                        title={product.title}
-                        price={product.price}
-                        image={product.image}
-                    />
-                )
-                )
-            }
-            </div>
+  return (
+    <aside
+      className={`${
+        isCheckoutSideMenuOpen
+          ? "opacity-100 right-2"
+          : "opacity-0 right-[-380px]"
+      } flex flex-col fixed  top-[80px] p-4 border bg-white/60 backdrop-blur-3xl border-gray-400 rounded-lg w-[360px] h-[calc(100vh-90px)] transition-all duration-[3000] overflow-y-auto `}
+    >
+      <div className="flex justify-between items-center pb-6 px-2">
+        <h2 className="font-medium text-xl">My Order</h2>
+        <XCircleIcon
+          className="h-6 w-6 text-gray-500 cursor-pointer hover:text-red-600 transition-all"
+          onClick={() => closeCheckoutSideMenu()}
+        />
+      </div>
 
-
-
-        </aside>
-    )
+      <div
+        className="overflow-y-auto scroll-smooth  [&::-webkit-scrollbar]:w-2
+                    [&::-webkit-scrollbar-track]:rounded-full
+                    [&::-webkit-scrollbar-track]:bg-gray-100
+                    [&::-webkit-scrollbar-thumb]:rounded-full
+                    [&::-webkit-scrollbar-thumb]:bg-gray-300"
+      >
+        {cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            image={product.image}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </aside>
+  );
 };
 
 export { CheckoutSideMenu };
--- curso-react-vite-tailwind/src/Components/OrderCard/index.jsx
+++ curso-react-vite-tailwind/src/Components/OrderCard/index.jsx
@@ -1,7 +1,7 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
 const OrderCard = props => {
-    const { id, title, image, price } = props;
+    const { id, title, image, price, handleDelete } = props;
     return (
         <div className="flex justify-between item-center mb-4">
             <div className="flex items-center gap-2">
@@ -14,7 +14,7 @@ const OrderCard = props => {
 
             <div className="flex items-center gap-2">
                 <p className="font-medium text-lg">$ {price}</p>
-                <XMarkIcon className="h-6 w-6 text-black cursor-pointer hover:text-red-500"/>
+                <XMarkIcon className="h-6 w-6 text-black cursor-pointer hover:text-red-500" onClick={() => handleDelete(id)}/>
             </div>
 
         </div>
